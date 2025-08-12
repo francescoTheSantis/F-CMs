9ab3f4ba08d66bf754e3ad53ca007f0c2dda4cd0
francescoTheSantis <dexo115bis@gmail.com>
2025-08-12 13:17:16 +0200

    update plot utils

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -49,7 +49,7 @@ def average_over_seed(input):
     std = {k: np.std(v) for k, v in d.items()}
     return mean, std
 
-def plot_single_c_on_y(
+def single_c_plot(
     input,
     custom_order,
     model_styles,
@@ -61,17 +61,17 @@ def plot_single_c_on_y(
     legend_size=14,
     legend_bgcolor='lightgray',
     legend_edgecolor='black',
-    legend_alpha=0.3
+    legend_alpha=0.3,
+    plot_name="default"
 ):
-    input = input[['seed', 'dataset', 'model', 'learning', 'single_c_interventions_on_y']].dropna()
-    input['single_c_interventions_on_y'] = input['single_c_interventions_on_y'].apply(delta_single_c_interventions_on_y)
 
     datasets = input['dataset'].unique()
     # Reorder datasets according to custom order
     datasets = sorted(datasets, key=lambda x: custom_order.index(x) if x in custom_order else len(custom_order))
     learning_methods = input['learning'].unique()
 
-    learning_methods = reorder(learning_methods) 
+    if plot_name=='single_c_interventions_on_y':
+        learning_methods = reorder(learning_methods)
 
     n_rows = len(learning_methods)
     n_cols = len(datasets)
@@ -93,7 +93,7 @@ def plot_single_c_on_y(
                 if model['name'] in subset['model'].values:
                     model_subset = subset[subset['model'] == model['name']]
                     if not model_subset.empty:
-                        interventions, _ = average_over_seed(model_subset['single_c_interventions_on_y'])
+                        interventions, _ = average_over_seed(model_subset[plot_name])
                         all_x_labels.update(interventions.keys())
             
             x_labels = sorted(list(all_x_labels))
@@ -110,7 +110,7 @@ def plot_single_c_on_y(
                 if model_subset.empty:
                     continue
 
-                interventions, interventions_std = average_over_seed(model_subset['single_c_interventions_on_y'])
+                interventions, interventions_std = average_over_seed(model_subset[plot_name])
                 
                 # Calculate offset for this specific model
                 offset = (model_idx - (len(model_styles) - 1) / 2) * bar_width
@@ -127,14 +127,22 @@ def plot_single_c_on_y(
                     width=bar_width,
                     label=model['name'],
                     color=color,
-                    alpha=0.7
+                    alpha=0.7,
+                    error_kw={'elinewidth': 0.5}  # Reduce the thickness of the error bars
                 )
 
                 if i == 0 and j == 0:
                     handles_labels.append(([bars[0], model['name']]))
 
-            ax.set_xticks(x)
-            ax.set_xticklabels(x_labels, rotation=45, ha='right', fontsize=tick_size)
+            if len(x_labels) > 15:
+                # Convert string IDs to numbers
+                x_labels_numeric = list(range(len(x_labels)))
+                ax.set_xticks(x_labels_numeric[::5])  # Show a tick every 5 ticks
+                ax.set_xticklabels(x_labels_numeric[::5], rotation=0, ha='right', fontsize=tick_size)
+            else:
+                ax.set_xticks(x)
+                ax.set_xticklabels(x_labels, rotation=45, ha='right', fontsize=tick_size)
+            
             ax.tick_params(axis='y', labelsize=tick_size)
             ax.minorticks_off()
             ax.grid(True)
@@ -149,7 +157,8 @@ def plot_single_c_on_y(
             else:
                 ax.set_title("")
 
-            ax.set_ylabel("$\\Delta$ on $y$", fontsize=label_size)
+            if j==0:
+                ax.set_ylabel("$\\Delta$ on $y$", fontsize=label_size)                
 
         # Get average vertical position of current row
         row_axes = [axes[i * n_cols + j] for j in range(n_cols)]
@@ -161,7 +170,8 @@ def plot_single_c_on_y(
         x_pos = leftmost_ax.x0 - 0.04  # Decrease this to get closer (0.02–0.03 usually works well)
 
         # Rename the learning methods
-        learning_method = rename_learning_methods([learning_method])[0]
+        if plot_name == 'single_c_interventions_on_y':
+            learning_method = rename_learning_methods([learning_method])[0]
 
         fig.text(
             x_pos,
@@ -192,21 +202,16 @@ def plot_single_c_on_y(
     plt.tight_layout(rect=[0.08, 0.07, 1, 1])
 
     if folder:
-        plt.savefig(f"{folder}/single_c_interventions_on_y.pdf", bbox_inches='tight')
+        plt.savefig(f"{folder}/{plot_name}.pdf", bbox_inches='tight')
     else:
         raise ValueError("Folder path is required to save the figure.")
 
-def delta_single_c_interventions_on_y_id_ood(d, base):
-    baseline = base['_baseline']
-    delta_dict = {k:(v - baseline) for k, v in d[0].items()}
-    return delta_dict
 
-def plot_single_id_ood_on_y(
-    input, 
+def plot_single_c_on_y(
+    input,
     custom_order,
     model_styles,
     folder=None,
-    id=False,
     figsize=(20, 15),
     title_size=16,
     label_size=14,
@@ -216,26 +221,84 @@ def plot_single_id_ood_on_y(
     legend_edgecolor='black',
     legend_alpha=0.3
 ):
+    input = input[['seed', 'dataset', 'model', 'learning', 'single_c_interventions_on_y']].dropna()
+    input['single_c_interventions_on_y'] = input['single_c_interventions_on_y'].apply(delta_single_c_interventions_on_y)
+    single_c_plot(
+        input,
+        custom_order,
+        model_styles,
+        folder=folder,
+        figsize=figsize,
+        title_size=title_size,
+        label_size=label_size,
+        tick_size=tick_size,
+        legend_size=legend_size,
+        legend_bgcolor=legend_bgcolor,
+        legend_edgecolor=legend_edgecolor,
+        legend_alpha=legend_alpha,
+        plot_name="single_c_interventions_on_y"
+    )
+
+
+def delta_single_c_interventions_on_y_id_ood(d, base):
+    baseline = base['_baseline']
+    delta_dict = {}
+    for k, v in base.items():
+        if k =='_baseline':
+            continue
+        if k in d:
+            delta_dict[k] = d[k] - baseline
+        if k not in d:
+            delta_dict[k] = 0
+
+    return delta_dict
+
+def plot_single_id_ood_on_y(
+    input, 
+    custom_order,
+    model_styles,
+    folder=None,
+    id=False,
+    figsize=(15, 10),
+    title_size=16,
+    label_size=14,
+    tick_size=14,
+    legend_size=14,
+    legend_bgcolor='lightgray',
+    legend_edgecolor='black',
+    legend_alpha=0.3
+):
     str_id = 'id' if id else 'ood'
+    plot_name = f"single_c_interventions_on_y_{str_id}"
+
     input = input[['seed', 'dataset', 'model', 'learning', f'single_{str_id}_on_y', 'single_c_interventions_on_y']].dropna()
+    # Explode the 'single_id_on_y' column to create multiple rows for each list element
+    input = input.explode(f'single_{str_id}_on_y').reset_index(drop=True)
+
+    # Update the 'learning' column with the index of the element in the list
+    input['learning'] = input.groupby(['seed', 'dataset', 'model', 'learning']).cumcount().apply(lambda x: f"cl. {x}")
 
     input[f'single_{str_id}_on_y'] = input.apply(
         lambda row: delta_single_c_interventions_on_y_id_ood(row[f'single_{str_id}_on_y'], row['single_c_interventions_on_y']), 
         axis=1
     )
-    #input[f'single_{str_id}_on_y'].apply(delta_single_c_interventions_on_y)
-    datasets = input['dataset'].unique()
-    # Reorder datasets according to custom order
-    datasets = sorted(datasets, key=lambda x: custom_order.index(x) if x in custom_order else len(custom_order))
-    learning_methods = input['learning'].unique()
 
-    learning_methods = reorder(learning_methods) 
 
-    n_rows = len(learning_methods)
-    n_cols = len(datasets)
+    # Rename f'single_{str_id}_on_y' to f"single_c_interventions_on_y_{str_id}"
+    input = input.rename(columns={f'single_{str_id}_on_y': f"single_c_interventions_on_y_{str_id}"})
 
-    fig = plt.figure(figsize=figsize)
-    gs = fig.add_gridspec(n_rows, n_cols, hspace=0.4, wspace=0.4)
-    axes = [fig.add_subplot(gs[i, j]) for i in range(n_rows) for j in range(n_cols)]
-
-    handles_labels = []
+    single_c_plot(
+        input,
+        custom_order,
+        model_styles,
+        folder=folder,
+        figsize=figsize,
+        title_size=title_size,
+        label_size=label_size,
+        tick_size=tick_size,
+        legend_size=legend_size,
+        legend_bgcolor=legend_bgcolor,
+        legend_edgecolor=legend_edgecolor,
+        legend_alpha=legend_alpha,
+        plot_name=plot_name
+    )
