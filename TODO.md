342a582fd1fa031bbb7fd0fdda7559c2794f0d54
francescoTheSantis <dexo115bis@gmail.com>
2025-10-24 17:07:43 +0200

    todo and plot utils updated

--- TODO.md
+++ TODO.md
@@ -1,8 +1,16 @@
 # TODO
 
-- [ ] concept accuracy
-- [ ] no worst case ma coverage
-- [ ] NO "single_c_interventions_on_y_clients_perspective"
-- [ ] numero unico per quantificare la bontà degli interventi del federato w.r.t. centralized
-- [ ] unobserved concept accuracy table
-- [ ] task NON necessariamente in ogni grafo (da ri-runnare cambiando)
+**Ari & Fra**
+1. **IMPORTANTE**!!!!!: dobbiamo uniformare il calcolo concept accuracy. Per ora calcoliamo la concept accuracy su tutti i concetti per cbm mentre calcoliamo la concept accuracy sui concetti parenti del task per i metodi graph based. cosa facciamo? facciamo predire al modello con il grafo anche i concetti non parenti del task? **Lasciare il bottleneck invariato per CBM ed eliminare i concetti non parent del task per C2BM. Poi calcolare la concept accuracy solo sui concetti presi da C2BM.**
+2. Controllare risultati sospetti di CGM e C2BM su Alarm. Far girare tutti gli esperimenti (localized e federated).
+3. modificare lo spit dei grafi:
+    3.1 i client non devono necessariamente avere l'annotazione sul task.
+    3.2 il numero di client e numero di sottografi sono due cose distinte (già implementato in questo modo)
+    3.3 i sottografi devono sempre avere almeno un nodo che non appartiene agli altri sottografi. 
+4. Ci serve un numero unico che quantifichi la bontà degli interventi del federato, per poi confrontarlo con il centralized (e con il localized).
+5. Manca da visualizzare la unobserved concept accuracy table. Calcolata partendo dai risultati del federated, mettendosi dal punto di vista dei vari client.
+6. Aggiungere datasets:
+    6.1 selezionare solo un task dal dataset multimodale.
+
+**Dario**
+...
--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1330,29 +1330,32 @@ def load_exps(exps_path, n_clients=5, args=None):
                 # Use the first matching graph
                 performance.at[idx, 'graph'] = matching_rows.iloc[0]['graph']
 
-    def _format_results(row, graph, dataset, model, count_nan=False, task=None):
+    def _format_results(row, graph, dataset, model, count_nan=False, task=None, worst_classifier=False):
         if model != 'blackbox':
             values = [v for k,v in row.items() if k in row]
             if count_nan:
                 return sum([1 for x in values if math.isnan(x)]) if values else 0
             else:
-                # replace the NaN values with the performance of the worst classifier
-                if dataset is not None and dataset in c_info and c_info[dataset] is not None:
-                    for idx, c_name in enumerate(graph):
-                        concept_cardinality_idx = c_info[dataset]['names'].index(c_name)
-                        concept_cardinality = c_info[dataset]['cardinality'][concept_cardinality_idx]
-                        if math.isnan(values[idx]):
-                            values[idx] = 1.0/concept_cardinality
+                if worst_classifier:
+                    # replace the NaN values with the performance of the worst classifier
+                    if dataset is not None and dataset in c_info and c_info[dataset] is not None:
+                        for idx, c_name in enumerate(graph):
+                            concept_cardinality_idx = c_info[dataset]['names'].index(c_name)
+                            concept_cardinality = c_info[dataset]['cardinality'][concept_cardinality_idx]
+                            if math.isnan(values[idx]):
+                                values[idx] = 1.0/concept_cardinality
+                else:
+                    # eliminate nan values before computing the average
+                    values = [x for x in values if not math.isnan(x)]
                 if task is not None:
                     values = values + [task]
                 return np.mean([x for x in values if not math.isnan(x)]) if values else 0
         else:
             return 0
 
-
     # Aggregate the results in concept_acc and task_acc for each row.
-    # For the localized training we ELIMINATE the NaN values for the OOD concepts and substitute those 
-    # values with the performance of the worst classifier (e.g., if binary concept, then 50% accuracy).
+    # In the aggregation, we simply compute the expected value over the concepts on which
+    # the model was trained. Therefore, if the model didn't train on a concept (NaN value), we ignore it in the average.
     performance['concept_left_out'] = performance.apply(lambda x: _format_results(x['concept_acc'], x['graph'], x['dataset'], x['model'], count_nan=True), axis=1)
     performance['agg_concept'] = performance.apply(lambda x: _format_results(x['concept_acc'], x['graph'], x['dataset'], x['model'], count_nan=False), axis=1)
     performance['agg_label'] = performance.apply(lambda x: _format_results(x['concept_acc'], x['graph'], x['dataset'], x['model'], count_nan=False, task=x['task_acc']), axis=1)
